43ebfeba934984cddda98b49efc6a47be72bda29
TakayukiOshiro <oosiaries@gmail.com>
2022-11-12 21:59:21 +0900

    todo_list.htmlの作成

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
 
-# Create your views here.
-def index(request):
-    return HttpResponse("<h1>Hello world</h1>")
+from .models import Todo
+
+class TodoList(ListView):
+    model = Todo
+    context_object_name = "tasks"
