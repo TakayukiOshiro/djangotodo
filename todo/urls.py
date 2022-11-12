a98c9ebd6fdc57e770c946ff0480f874de40e697
TakayukiOshiro <oosiaries@gmail.com>
2022-11-12 22:16:11 +0900

    詳細を表示させ、押下したら詳細を表示するページに遷移するように変更

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TodoList
+from .views import TodoDetail, TodoList, DetailView
 
 urlpatterns = [
     path("", TodoList.as_view(), name="list"),
+    path("detail/<int:pk>", TodoDetail.as_view(), name="detail")
 ]
